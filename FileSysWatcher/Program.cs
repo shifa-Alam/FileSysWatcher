ad17dd516d01b01e8382329e8f7e4bfc876dec4a
Shifa Alam <shifa.alam@rite.com.bd>
2021-11-24 17:45:02 +0600

    updated

--- FileSysWatcher/Program.cs
+++ FileSysWatcher/Program.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.IO;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace FileSysWatcher
 {
     internal class Program
     {
-        static string path = @"C:\Users\test\Desktop\test";
+        static string path = @"C:\Users\shifa\Desktop\test";
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-          
+
             MonitorDirectory(path);
             Console.ReadKey();
         }
@@ -28,17 +30,31 @@ namespace FileSysWatcher
 
         }
 
-      
+
 
         private static void FileSystemWatcher_Created(object sender, FileSystemEventArgs e)
         {
+            //dataTable.Columns[i].ColumnName = "SubjectMarks";
+            var reader = new System.IO.StreamReader(e.FullPath, System.Text.Encoding.UTF8);
+            var text = reader.ReadToEnd();
+
+            var fileData = new CustomFileInputModel()
+            {
+                Name = e.Name,
+                FileData = text,
+                DocTypeId = 17,
+                Type = e.GetType().ToString()
+        };
 
             var excel = new ExcelInputModel();
+            excel.TenantId = 2;
+            excel.BranchId = 2;
+            excel.HrConfigId = 2;
+            
+            excel.CustomFile = fileData;
 
-            //excel.CustomFile.Name = e.Name;
-            //excel.CustomFile.Size = e.Siz;
-            //File.Move(e.FullPath, path+"\\x\\"+e.Name);
-            _ = CallWebAPIAsync(e.FullPath);
+
+            _ = CallWebAPIAsync(excel);
 
             Console.WriteLine("File Created :{0}", e.Name);
         }
@@ -46,25 +62,65 @@ namespace FileSysWatcher
         {
             Console.WriteLine("File Renamed :{0}", e.Name);
         }
-       
+
         private static void FileSystemWatcher_Deleted(object sender, FileSystemEventArgs e)
         {
             Console.WriteLine("File Deleted :{0}", e.Name);
         }
 
 
-        private static  async Task CallWebAPIAsync(string path)
+        private static async Task CallWebAPIAsync(ExcelInputModel data)
         {
-            var student = "{'Id':'1','Name':'Steve'}";
+
+            //var token = GetToken();
 
            
 
+
+
+            //var student = "{'Id':'1','Name':'Steve'}";
+
+            var opt = new JsonSerializerOptions() { WriteIndented = true };
+            
+            string strJson = JsonSerializer.Serialize<ExcelInputModel>(data, opt);
+
+
+
             HttpClient client = new HttpClient();
             client.BaseAddress = new Uri("http://localhost:44317/");
-            var response = await client.PostAsync("api/HrAttendanceTest/ImportAttendanceAsync", new StringContent(student, Encoding.UTF8, "application/json"));
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await client.PostAsync("api/HrAttendanceTest/ImportAttendanceAsync", new StringContent(strJson, Encoding.UTF8, "application/json"));
+           
             if (response != null)
             {
                 Console.WriteLine(response.ToString());
+            }
+        }
+
+        private async static Task<string> GetToken()
+        {
+            try
+            {
+                var body = $"username =manager@demo.com&password=admin123##&grant_type=password";
+
+                HttpClient loginCall = new HttpClient();
+                loginCall.BaseAddress = new Uri("http://localhost:44317/");
+
+                var token = await loginCall.PostAsync("token", new StringContent(body, Encoding.UTF8, "application/json"));
+                if (token != null)
+                {
+                    Console.WriteLine(token.ToString());
+                }
+
+                return "token";
+
+            }
+            catch (Exception)
+            {
+
+                throw;
             }
         }
     }
